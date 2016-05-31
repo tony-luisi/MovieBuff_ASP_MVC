2d7bbc92cdc0ce19b158697bc0e0a655bcc22cf0
Tony Luisi <t.luisi@gmail.com>
2016-06-01 00:02:14 +0100

    Add project files.

---
+++ MovieBuff_ASP_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieBuff_ASP_MVC.MvcApplication" Language="C#" %>
---
+++ MovieBuff_ASP_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieBuff_ASP_MVC.Startup))]
+namespace MovieBuff_ASP_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
